63b624c55eed97eb180dd3f5af82aa3e9bfb5474
jkdzzzz <pascal.pri0502@gmail.com>
2025-03-13 19:56:44 +0100

    Updated File I/O

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
-use std::fs::File;
-use std::io::{self, Write};
+use std::fs::OpenOptions;
+use std::io::{self, Write, BufRead};
 use std::process;
 
 fn main() {
     let mut new_task = String::new();
-    let mut file = match File::create("output.txt") {
+    let path = "list.txt";
+    let mut file = match OpenOptions::new().append(true).write(true).read(true).create(true).open(path) {
         Ok(file) => file,
         Err(e) => {
-            eprintln!("Error creating the file: {e}");
+            eprintln!("Failed to open / create {path}: {e}");
             process::exit(1);
         }
     };
 
+    let reader = io::BufReader::new(&file);
+    let mut tasks: Vec<String> = Vec::new();
+
+    for line in reader.lines() {
+        match line {
+            Ok(l) => tasks.push(l),
+            Err(e) => {
+                eprintln!("Error reading file: {e}");
+                break;
+            },
+        }
+    }
+
     println!("Simple Rust ToDo-List");
+    
+    for task in tasks {
+        println!("{task}");
+    }
+
     print!("New Task: ");
     io::stdout().flush().expect("Failed to flush stdout!");
     io::stdin().read_line(&mut new_task).expect("The input is invalid!");
 
-    if let Err(e) = file.write_all(new_task.as_bytes()) {
+    new_task = new_task.trim().parse().expect("The input is not a valid string!");
+
+    if let Err(e) = writeln!(file, "{new_task}") {
         eprintln!("Error writing to file: {e}");
         process::exit(1);
     }
 
-    println!("Successfully written Task to output.txt");
+    println!("Successfully written Task to {path}");
 }
